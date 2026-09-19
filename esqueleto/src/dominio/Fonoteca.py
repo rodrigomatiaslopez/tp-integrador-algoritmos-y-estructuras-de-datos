e007ef7bd6752cb62d48280dab9d8f76ec1028a4
rodrigomatiaslopez <rodrigodark1234@gmail.com>
2026-09-19 11:50:01 -0300

    actualizacion canciones
    se agregaron 4 canciones con sus distintas versiones.

--- esqueleto/src/dominio/Fonoteca.py
+++ esqueleto/src/dominio/Fonoteca.py
@@ -4,4 +4,13 @@ fonoteca = [
     {"nombre" : "El tren del cielo", "autor": "Soledad Pastorutti", "album": "Libre", "genero": "Folklore"},
     {"nombre" : "Redemption Song", "autor": "Bob Marley and The Wailers", "album": "Uprising", "genero": "Roots Reggae"},
     {"nombre" : "The Trooper", "autor": "Iron Maiden", "album": "Piece of Mind", "genero": "Heavy Metal"}
+    {"nombre" : "De musica ligera", "autor" : "Soda Stereo", "album" : "Cancion Animal", "genero" : "Rock", "versiones": [
+        {"nombre" : "De musica ligera (Unplugged)", "autor" : "Soda Stereo", "album" : "Comfort y Musica Para Volar", "genero" : "Rock"}]},
+    {"nombre" : "Jijiji", "autor" : "Patricio Rey y sus Redonditos de Ricota", "album" : "Un Baion para el Ojo Idiota", "genero" : "Rock", "versiones": [
+        {"nombre" : "Jijiji", "autor" : "Patricio Rey y sus Redonditos de Ricota", "album" : "En Directo", "genero" : "Rock"}]},
+    {"nombre" : "Flaca", "autor" : "Andres Calamaro", "album" : "Honestidad Brutal", "genero" : "Rock", "versiones": [
+        {"nombre" : "Flaca", "autor" : "Andres Calamaro", "album" : "Honestidad Brutal", "genero" : "Rock"}]},
+    {"nombre" : "Bohemian Rhapsody", "autor" : "Queen", "album" : "A Night at the Opera", "genero" : "Rock", "versiones": [
+        {"nombre" : "Bohemian Rhapsody (Live Aid)", "autor" : "Queen", "album" : "Live Aid", "genero" : "Rock"}
+    ]}
 ]
