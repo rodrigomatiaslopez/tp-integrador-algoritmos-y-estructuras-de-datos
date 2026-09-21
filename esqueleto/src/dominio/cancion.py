9eddb53e572a0af4158b069900af48955a41907c
rodrigomatiaslopez <rodrigodark1234@gmail.com>
2026-09-20 21:59:01 -0300

    Update cancion.py
    se agrega id al resumen

--- esqueleto/src/dominio/cancion.py
+++ esqueleto/src/dominio/cancion.py
@@ -7,5 +7,5 @@ class Cancion: #Clase basica de una cancion, define su id, nombre, autor, album 
         self.genero = genero
 
     def __str__(self): #Cuando se ejecuta print() sobre una canciones, se corre esta funcion
-        resumen = self.nombre + " | " + self.autor + " | " + self.album + " | " + self.genero
+        resumen = self.cancion_id + " | " + self.nombre + " | " + self.autor + " | " + self.album + " | " + self.genero
         return resumen
